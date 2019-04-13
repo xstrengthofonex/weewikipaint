64429bbf52a48a8e77402d8fde7968e5dc40990b
Vincent White <xstrengthofonex@gmail.com>
2019-04-13 21:28:18 +0900

    Setup strict lint options

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,35 @@
+/*global desc, task, jake, fail, complete*/
+
+"use strict";
+
 task("default", ["lint"], function(){});
 
 desc("Linting");
 task("lint", [], function(){
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js", {}, {});
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    lint.validateFileList(files.toArray(), nodeLintOptions(), {});
 });
+
+function nodeLintOptions() {
+	return {
+		bitwise: true,
+		curly: false,
+		eqeqeq: true,
+		forin: true,
+		immed: true,
+		latedef: false,
+		newcap: true,
+		noarg: true,
+		noempty: true,
+		nonew: true,
+		regexp: true,
+		undef: true,
+		strict: true,
+		trailing: true,
+		node: true
+	};
+}
+
